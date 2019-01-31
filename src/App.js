da7c583b89878566a26a6367234a5adda6cd9e0c
yangheng <yangheng@ireadercity.com>
2019-01-31 16:33:49 +0800

    啊啊啊

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import {Header} from './containers/header';
+import Swiper from './components/swiper';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
+		const options = {
+		  direction: 'horizontal',
+			interval: 300
+    };
+		return (
       <div className="App">
         <Header title={'title'}></Header>
+				<Swiper {...options}>
+					<div className="s-item">slide 1</div>
+					<div className="s-item">slide 2</div>
+					<div className="s-item">slide 3</div>
+					<div className="s-item">slide 4</div>
+				</Swiper>
       </div>
     );
   }
